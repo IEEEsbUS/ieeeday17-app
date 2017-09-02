2cb4a83ee7da7717fda742531caff63ba990f1bf
merelo <merelo.miguel@gmail.com>
2017-09-02 23:08:08 +0200

    Basic info to AgendaPage added

--- src/pages/agenda/agenda.ts
+++ src/pages/agenda/agenda.ts
@@ -12,21 +12,45 @@ export class AgendaPage {
   }
 
   showAlert(dia) {
-   if(dia==119){
+    if(dia==1){
 	let alert = this.alertCtrl.create({
-	    title: 'Viernes 19:00',
-	    subTitle: 'Reunión de capítulos técnicos<br/><img src="assets/images/aess.png" width:50px/>',
+	    title: 'Viernes 16:00',
+	    subTitle: '<b>Presentación de IEEE y de la Sección Española</b><br/><br/>Explicación de lo que es IEEE y la Sección Española y de las tareas que llevan a cabo',
 	    buttons: ['OK']
     	});
 	alert.present();
-   }
-   else if(dia==120){
+    }
+    else if(dia==2){
    	let alert = this.alertCtrl.create({
-	    title: 'Viernes 20:00',
-	    subTitle: 'Otra cosa prueba de maxima capacidad para ver como REACCIONA ESTA MIERDA ASIQUE NANANAN BATAMAN Y BATMAN TAMBIEN',
+	    title: 'Viernes 17:00',
+	    subTitle: '<b>Charla sobre Industry Networking</b><img src="assets/images/andreasNEUMEIER.jpg"/><b>Andreas Neumeier</b><br/>Manager  para la estrategia de IoT en OSRAM/Sylvania',
 	    buttons: ['OK']
     	});
 	alert.present();
-   }
-   }
+    }
+    else if(dia==3){
+   	let alert = this.alertCtrl.create({
+	    title: 'Viernes 18:00',
+	    subTitle: '<b>Reconocimiento a miembros</b>',
+	    buttons: ['OK']
+    	});
+	alert.present();
+    }
+    else if(dia==4){
+   	let alert = this.alertCtrl.create({
+	    title: 'Viernes 19:00',
+	    subTitle: '<b>Panel debate sobre la industria en España</b>',
+	    buttons: ['OK']
+    	});
+	alert.present();
+    }
+    else if(dia==5){
+   	let alert = this.alertCtrl.create({
+	    title: 'Viernes 19:45',
+	    subTitle: '<b>Entrega de premios de la Sección Española</b>',
+	    buttons: ['OK']
+    	});
+	alert.present();
+    }
+  }
 }
