bcc7a6981b77f3e119cdeec3799eb2c7f404eab0
merelo <merelo.miguel@gmail.com>
2017-09-24 09:22:20 +0200

    Añadidas modificaciones a algunas ramas, easter egg añadido

--- src/pages/ramas-ieee/ramas-ieee.ts
+++ src/pages/ramas-ieee/ramas-ieee.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController,AlertController } from 'ionic-angular';
+import { NativeAudio } from '@ionic-native/native-audio';
 
 import { RamasDetalle } from '../ramas-detalle/ramas-detalle';
 
@@ -19,7 +20,7 @@ export class RamasIEEE {
   
   sbUA={title:"UA",imagen:"assets/images/ramas/ieeeUA.png",ciudad:"Universidad de Alicante",twitter:"ieee_sbua",facebook:"ieeeuasb",instagram:"ieee_sbua",web:"http://sites.ieee.org/sb-alicante/es/inicio/"};
   
-  sbUC3M={title:"UC3M",imagen:"assets/images/ramas/ieeeUC3M.png",ciudad:"Universidad Carlos III de Madrid",twitter:"asrob_uc3m",facebook:"asrobuc3m",web:"http://asrob.uc3m.es/index.php/Main_Page"};
+  //sbUC3M={title:"UC3M",imagen:"assets/images/ramas/ieeeUC3M.png",ciudad:"Universidad Carlos III de Madrid",twitter:"asrob_uc3m",facebook:"asrobuc3m",web:"http://asrob.uc3m.es/index.php/Main_Page"};
   
   sbUGR={title:"UGR",imagen:"assets/images/ramas/ieeeUGR.png",ciudad:"Universidad de Granada",twitter:"ieeegranada",facebook:"IEEEGranada",web:"http://sites.ieee.org/sb-ugr/"};
   
@@ -30,7 +31,7 @@ export class RamasIEEE {
   
   sbUPCT={title:"UPCT",imagen:"assets/images/ramas/ieeeUPCT.png",ciudad:"Universidad Politécnica de Cartagena",twitter:"ieeesupct",facebook:"ieeeupct",instagram:"ieeeupct",web:"http://www.ieee.upct.es/"};
   
-  sbBCN={title:"UPC",imagen:"assets/images/ramas/ieeeUBCN.png",ciudad:"Universidad Politécnica de Cataluña",twitter:"IEEE_SB_BCN",facebook:"IEEEBcnStudentBranch",web:"http://ieee.upc.edu/"};
+  sbBCN={title:"UPC",imagen:"assets/images/ramas/ieeeUBCN.png",ciudad:"Universidad Politécnica de Cataluña",twitter:"IEEE_SB_BCN",facebook:"IEEEBcnStudentBranch",instagram:"ieeebcnsb",web:"http://ieee.upc.edu/"};
   
   sbUPMN={title:"UPM",imagen:"assets/images/ramas/ieeeUPM.png",ciudad:"Universidad Politécnica de Madrid",twitter:"ieeesb",facebook:"IeeeSbMadrid",web:"http://ieeesb.es"};
   
@@ -47,14 +48,39 @@ export class RamasIEEE {
   sbUVigo={title:"UVIGO",imagen:"assets/images/ramas/ieeeUVigo.png",ciudad:"Universidad de Vigo",twitter:"IEEEuVigo",facebook:"ieeevigo"};
   
   sbUZ={title:"UNIZAR",imagen:"assets/images/ramas/ieeeUZ.png",ciudad:"Universidad de Zaragoza",twitter:"ieeesbuz",facebook:"IEEEsbUZ",web:"http://sites.ieee.org/sb-unizar/"};
-  
+
+  cuenta=0;
   
 
-  constructor(public navCtrl: NavController,private alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController,private alertCtrl: AlertController,private nativeAudio: NativeAudio) {
+    this.nativeAudio.preloadSimple('alerta', 'assets/sounds/alerta.mp3');
+    this.cuenta=0;
+  }
+
+  ionViewDidEnter(){
+    if(this.cuenta==5)
+	this.sonido();
   }
 
   newBranch(titulo){
+	this.cuenta++;
 	this.navCtrl.push(RamasDetalle,titulo);
+  }
+
+  reproducir(){
+    this.nativeAudio.play('alerta');
+  }
+
+  sonido(){
+    var cuadro=document.getElementById("cuadro");
+    cuadro.style.animationName="cuadro";
+    cuadro.style.webkitAnimationName="cuadro";
+    cuadro.style.display="inline";
+    //cuadro.style.webkitAnimationPlayState="running";
+    setTimeout(this.reproducir(), 500);
+    setTimeout(function(){
+      cuadro.style.display="none";
+    }, 2500);
   }
 
 }
